143f96b81bbc3fc8a6b5f9257d380498798afdd7
Caleb-J-Wallace <124644107+Caleb-J-Wallace@users.noreply.github.com>
2026-02-05 19:14:45 -0500

    Add climber CANCoder as zeroing device.
    Add climber CANCoder.
    Set it as zeroing device.
    Move motor and CANCoder configs into helper methods.

--- src/main/java/org/team340/robot/subsystems/Climber.java
+++ src/main/java/org/team340/robot/subsystems/Climber.java
@@ -1,13 +1,17 @@
 package org.team340.robot.subsystems;
 
 import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
+import com.ctre.phoenix6.signals.ReverseLimitTypeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.team340.lib.util.command.GRRSubsystem;
 import org.team340.lib.util.vendors.PhoenixUtil;
@@ -17,30 +21,21 @@ public class Climber extends GRRSubsystem {
 
     private final TalonFX lead;
     private final TalonFX follow;
+    private final CANcoder zeroSwitch;
 
     private final PositionVoltage positionVoltage;
 
     public Climber() {
         this.lead = new TalonFX(RobotMap.CLIMBER_LEAD_MOTOR, RobotMap.CANBus);
         this.follow = new TalonFX(RobotMap.CLIMBER_FOLLOW_MOTOR, RobotMap.CANBus);
+        this.zeroSwitch = new CANcoder(RobotMap.CLIMBER_CANCODER, RobotMap.CANBus);
 
-        final TalonFXConfiguration config = new TalonFXConfiguration();
+        final CANcoderConfiguration cancoderConfig = createCANCoderConfig();
 
-        config.CurrentLimits.StatorCurrentLimit = 80.0;
-        config.CurrentLimits.SupplyCurrentLimit = 70.0;
+        PhoenixUtil.run(() -> zeroSwitch.clearStickyFaults());
+        PhoenixUtil.run(() -> zeroSwitch.getConfigurator().apply(cancoderConfig));
 
-        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-
-        config.Slot0.kP = 0.0;
-        config.Slot0.kI = 0.0;
-        config.Slot0.kD = 0.0;
-        config.Slot0.kG = 0.0;
-        config.Slot0.kS = 0.0;
-        config.Slot0.kV = 0.0;
-        config.Slot0.kA = 0.0;
-
-        // TODO: Find out the direction of the motor.
-        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        final TalonFXConfiguration config = createMotorConfig();
 
         PhoenixUtil.run(() -> lead.clearStickyFaults());
         PhoenixUtil.run(() -> lead.getConfigurator().apply(config));
@@ -78,5 +73,46 @@ public class Climber extends GRRSubsystem {
             .onEnd(() -> {
                 lead.stopMotor();
             });
+    }
+
+    private CANcoderConfiguration createCANCoderConfig() {
+        final CANcoderConfiguration config = new CANcoderConfiguration();
+
+        return config;
+    }
+
+    private TalonFXConfiguration createMotorConfig() {
+        final TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.CurrentLimits.StatorCurrentLimit = 80.0;
+        config.CurrentLimits.SupplyCurrentLimit = 70.0;
+
+        config.HardwareLimitSwitch.ReverseLimitRemoteSensorID = RobotMap.CLIMBER_CANCODER;
+        config.HardwareLimitSwitch.ReverseLimitSource = ReverseLimitSourceValue.RemoteCANcoder;
+        config.HardwareLimitSwitch.ReverseLimitType = ReverseLimitTypeValue.NormallyOpen;
+        config.HardwareLimitSwitch.ReverseLimitAutosetPositionEnable = true;
+        config.HardwareLimitSwitch.ReverseLimitAutosetPositionValue = 0.0;
+        config.HardwareLimitSwitch.ReverseLimitEnable = false;
+
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        config.Slot0.kP = 0.0;
+        config.Slot0.kI = 0.0;
+        config.Slot0.kD = 0.0;
+        config.Slot0.kG = 0.0;
+        config.Slot0.kS = 0.0;
+        config.Slot0.kV = 0.0;
+        config.Slot0.kA = 0.0;
+
+        config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.0;
+        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+
+        config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.0;
+        config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+
+        // TODO: Find out the direction of the motor.
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        return config;
     }
 }
