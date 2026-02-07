5ac3926b184cb8e921b9a77e1534e76aa06c7367
Caleb-J-Wallace <124644107+Caleb-J-Wallace@users.noreply.github.com>
2026-02-07 10:11:34 -0500

    Add manual control command to Shooters.

--- src/main/java/org/team340/robot/subsystems/Shooters.java
+++ src/main/java/org/team340/robot/subsystems/Shooters.java
@@ -1,5 +1,14 @@
 package org.team340.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
+import org.team340.lib.tunable.Tunables;
+import org.team340.lib.tunable.Tunables.TunableDouble;
+import org.team340.lib.util.Mutable;
+import org.team340.lib.util.command.GRRSubsystem;
+import org.team340.lib.util.vendors.PhoenixUtil;
+import org.team340.robot.Constants.RobotMap;
+
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
@@ -8,15 +17,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj2.command.Command;
-import java.util.function.DoubleSupplier;
-import org.team340.lib.util.command.GRRSubsystem;
-import org.team340.lib.util.vendors.PhoenixUtil;
-import org.team340.robot.Constants.RobotMap;
 
 public final class Shooters extends GRRSubsystem {
 
+    private static final TunableDouble manualAcceleration = Tunables.value("Shooters/manualAcceleration", 0.0);
     private static final InterpolatingDoubleTreeMap distanceVelocityMap;
 
     static {
@@ -101,6 +108,18 @@ public final class Shooters extends GRRSubsystem {
      */
     public Command targetDistance(final DoubleSupplier distance) {
         return runVelocity(() -> distanceVelocityMap.get(distance.getAsDouble())).withName("Shooters.targetDistance()");
+    }
+
+    /**
+     * Manually run the shooters faster or slower using a acceleration input.
+     * @param acceleration A acceleration between [-1.0, 1.0] to drive the hood up or down. This is scaled by {@link Shooters#manualSpeed}.
+     */
+    public Command manualControl(final DoubleSupplier acceleration) {
+        final Mutable<Double> velocity = new Mutable<>(0.0);
+
+        return runVelocity(() -> velocity.value += -acceleration.getAsDouble() * manualAcceleration.get())
+            .beforeStarting(() -> velocity.value = 0.0)
+            .withName("Shooters.manualControl()");
     }
 
     /**
